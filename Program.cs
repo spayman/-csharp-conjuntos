32b7c51f9c7e9814b27220807802e291077a5509
spayman <fpssouzza@gmail.com>
2019-07-18 23:14:37 -0300

    Acrestado comentários ao código

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace Exercicio_05_Conjunto
     {
 
         static void Main(string[] args)
-        {
+        { 
+            // Declaração dos conjuntos
             HashSet<int> A = new HashSet<int>();
             HashSet<int> B = new HashSet<int>();
             HashSet<int> C = new HashSet<int>();
@@ -35,7 +36,7 @@ namespace Exercicio_05_Conjunto
                 B.Add(vl);
             }
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-            // Dados do curso B
+            // Dados do curso C
             Console.Write("O curso C possui quantos alunos?: ");
             int c = int.Parse(Console.ReadLine());
             Console.WriteLine();
@@ -46,13 +47,14 @@ namespace Exercicio_05_Conjunto
                 C.Add(vl);
             }
 
-            Console.WriteLine();   
+            Console.WriteLine(); 
+            // Declara um conjunto 'tot' do tipo inteiro para armazenar os elementos em união de cada conjunto.
             HashSet<int> tot = new HashSet<int>();
-
+            // Inicia o processo de união de cada conjunto
             tot.UnionWith(A);
             tot.UnionWith(B);
             tot.UnionWith(C);
-
+            // Apresenta o resultado para o usuário
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             Console.WriteLine("O instrutor Joaquim tem um total de {0} alunos", tot.Count);
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
